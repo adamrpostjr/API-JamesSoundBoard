6b232fc805680165153d41f00ef1c1f0d6bb9f11
Adam Post <adamrpostjr@gmail.com>
2021-11-10 14:29:00 -0500

    cors sucks

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const cors = require('cors');
 const fs = require('fs')
 const app = express()
 app.use(cors({
-    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-    exposedHeaders: ["authorization"], // you can change the headers
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false
