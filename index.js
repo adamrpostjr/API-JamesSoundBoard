1cf7d91ae3fbdfb80cd01b729bea2299236f3610
Adam Post <adamrpostjr@gmail.com>
2021-11-10 14:16:45 -0500

    I HATE CORS

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs')
 const app = express()
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('sounds/'));
 app.use(bodyParser.json());
-app.use(cors())
 
 app.get('/list-sounds', async (req, res)=>{
    res.json(fs.readdirSync('./sounds'))
