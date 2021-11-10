939562b06e4a2d8028c06dc50279ff76b370740a
Adam Post <adamrpostjr@gmail.com>
2021-11-10 13:34:20 -0500

    created API for James Soundboard

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'tmp/' })
+const bodyParser = require('body-parser');
+const fs = require('fs')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('sounds/'));
+app.use(bodyParser.json());
+
+app.get('/list-sounds', async (req, res)=>{
+   res.json(fs.readdirSync('./sounds'))
+})
+
+app.get('/sounds/:file', async (req, res)=>{
+    res.sendFile(__dirname +`/sounds/${req.params.file}`)
+})
+
+app.post('/upload', upload.single('sound'), async (req, res)=>{
+    // this route is protected with a special key -- sorry people
+    if (req.body.pass != process.env.UPLOADKEY) {
+        res.status(401).json('sorry pal..')
+        return
+    }
+
+    if (req.file && req.file.mimetype == 'audio/mpeg') {
+
+        let existing = fs.readdirSync('./sounds')
+        if (existing.includes(req.file.originalname)) {
+            fs.unlink(`./tmp/${req.file.filename}`)
+            res.status(409).json('file exist')
+            return
+        }
+
+        fs.renameSync(`./tmp/${req.file.filename}`, `./sounds/${req.file.originalname}`)
+    }
+
+    res.json('thanks')
+})
+
+
+const server = app.listen(8085, function(){
+    console.log("Server started at 8085");
+});
